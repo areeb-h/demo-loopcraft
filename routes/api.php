9cc941b1a1dc858cfc3e5828b040293f9057ea97
areeb-h <areeb.hw@gmail.com>
2024-01-16 10:18:06 +0500

    Add image upload route [api.php]

--- routes/api.php
+++ routes/api.php
@@ -4,8 +4,15 @@
  * Areeb Hussain
  */
 
+use App\Filament\Resources\Shop\BrandResource;
+use App\Filament\Resources\Shop\CategoryResource;
+use App\Filament\Resources\Shop\CustomerResource;
+use App\Filament\Resources\Shop\OrderResource;
+use App\Filament\Resources\Shop\ProductResource;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ProductImageController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -31,11 +38,26 @@ Route::middleware('auth:sanctum')->group(function () {
 
 Route::middleware('auth:sanctum')->group(function () {
 
+    // Customer API routes defined individually
     Route::get('/customers', [CustomerController::class, 'index']);
     Route::post('/customers', [CustomerController::class, 'store']);
     Route::put('/customers/{customer}', [CustomerController::class, 'update']);
     Route::delete('/customers/{customer}', [CustomerController::class, 'destroy']);
-    
-});
 
+    // Products API route
+    Route::apiResource('products', ProductController::class);
+
+    // API Resource routes for Brands, Categories, and Orders.
+    // Automatically create standard endpoints for each resource.
+    Route::apiResource('brands', BrandResource::class);
+    Route::apiResource('categories', CategoryResource::class);
+    Route::apiResource('orders', OrderResource::class);
+
+    // This also works
+    // Route::apiResource('customers', CustomerController::class);
+
+    Route::post('/products/{product}/upload-image', [ProductImageController::class, 'uploadImage'])
+        ->name('api.products.upload-image');
+
+});
 
